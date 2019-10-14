77013c8493b6caeb188351048aa08c83d9b3a022
Daniella Kanj <d.kanj8339@student.leedsbeckett.ac.uk>
2019-10-14 10:10:41 +0000

    before csrf field

---
+++ app/Http/Controllers/ProjectsController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Project;
+
+use Illuminate\Http\Request;
+
+class ProjectsController extends Controller
+{
+    public function index()
+    {
+        $projects = Project::all();
+        
+        return $projects;
+        return view('projects.index', compact('projects'));
+    }
+    
+    public function create()
+    {
+        return view('projects.create');
+    }
+    
+    public function store()
+    {
+        return request()->all();
+    }
+}
